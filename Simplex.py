e98ad7671305dd32f805cdba9d1855b430e47c3f
PSameetHegdeGit <sameethegde123@gmail.com>
2020-04-15 15:01:32 -0400

    backend seems to work

--- Simplex.py
+++ Simplex.py
@@ -2,34 +2,41 @@ from numpyNotes import *
 import numpy as np
 
 def simplexSolver(matrix, basic):
-    basicVar = [basic[i].get() for i in range(len(basic))]
+    #basicVar = [basic[i].get() for i in range(len(basic))]
     print(matrix)
     columnLastIndex = len(matrix[0]) - 1
     rowLastIndex = len(matrix) - 1
 
-    print(basicVar)
-
-    objectiveRow = [matrix[i][columnLastIndex] for i in range(len(matrix))]
-    lastColumn = matrix[rowLastIndex]
+    #print(basicVar)
 
     while True:
+
+        #lastcolumn will =  a reference to matrix's last array
+        lastColumn = matrix[rowLastIndex]
+
+        objectiveRow = [matrix[i][columnLastIndex] for i in range(len(matrix))]
+        zValue = objectiveRow.pop()
+
         biggestNegativeIndex = find_min_of_obj(objectiveRow)
 
         if biggestNegativeIndex is None:
-            print("exiting...")
+            print("exiting...\n\n")
+            print("Final matrix is: " + str(matrix))
+            print("Final Solution: " + str(zValue))
+
             exit()
         else:
             print("in else")
             print("The Biggest Negative Index is: " + str(biggestNegativeIndex))
 
-            pivotColumnIndex = biggestNegativeIndex
-            pivotRowIndex = minimum_Theta_ratios(matrix, pivotColumnIndex, lastColumn)
+            pivot_ColumnIndex = biggestNegativeIndex
+            pivot_RowIndex = minimum_Theta_ratios(matrix, pivot_ColumnIndex, lastColumn)
 
-            print("The Pivot Index is: " + str(pivotRowIndex))
+            print("The Pivot Index is: " + str(pivot_RowIndex))
 
-            matrix = manipulatingRows(matrix, pivotRowIndex, pivotColumnIndex)
+            manipulatingRows(matrix, pivot_ColumnIndex, pivot_RowIndex)
 
-            exit()
+
 
 
     # TODO:CREATE A SIMPLEX SOLVER
@@ -41,8 +48,8 @@ def find_min_of_obj(objectiveRow):
     minimum = None
 
     #Last entry is z value
-    objectiveRow.pop()
-    print("Objective Row after Pop: " + str(objectiveRow))
+
+    print("Objective Row: " + str(objectiveRow))
 
     for minimumIndex in range(len(objectiveRow)):
         value = objectiveRow[minimumIndex]
@@ -66,7 +73,11 @@ def minimum_Theta_ratios(matrix, columnIndex, lastColumn):
     print("lastcolumn is: " + str(lastColumn))
 
     for i in range(len(columnEntries)):
-        thetavals.append(lastColumn[i]/columnEntries[i])
+        try:
+            value = lastColumn[i]/columnEntries[i]
+            thetavals.append(value)
+        except ZeroDivisionError:
+            thetavals.append(0)
 
     print(thetavals)
     #Minimum works for all generator objects
@@ -75,26 +86,32 @@ def minimum_Theta_ratios(matrix, columnIndex, lastColumn):
     return pivotRowIndex
 
 
-def manipulatingRows(matrix, pivot_RowIndex, pivot_ColumnIndex):
+def manipulatingRows(matrix, pivot_ColumnIndex, pivot_RowIndex):
     print("Manipulating rows")
-    pivotEntry = matrix[pivot_RowIndex][pivot_ColumnIndex]
+    print("Before transformation: " + str(matrix))
+
+    pivotEntry = matrix[pivot_ColumnIndex][pivot_RowIndex]
+
+    print("pivot Entry is:" + str(pivotEntry))
 
     #Part 1
-    for i in range(len(matrix)):
-        matrix[pivot_RowIndex][i] = matrix[pivot_RowIndex][i] / pivotEntry
+    for column in range(len(matrix)):
+        matrix[column][pivot_RowIndex] = (matrix[column][pivot_RowIndex] / pivotEntry)
 
-    print(matrix)
+    print("Transforming after part 1:" + str(matrix))
 
     #Part 2
-    for rowIndex in range(len(matrix[0])):
-        if rowIndex == pivot_RowIndex:
+    for row in range(len(matrix[0])):
+        if row == pivot_RowIndex:
             continue
         else:
-            pivotColumn_Entry = matrix[rowIndex][pivot_ColumnIndex]
-            multiple = -(pivotColumn_Entry/pivotEntry)
-            for columnIndex in range(len(matrix)):
-                matrix[rowIndex][i] = matrix[rowIndex][i] + multiple*matrix[rowIndex][columnIndex]
+            pivotColumn_Entry = matrix[pivot_ColumnIndex][row]
+            multiple = -1 * pivotColumn_Entry
+            for column in range(len(matrix)):
+                entry = matrix[column][row]
+                matrix[column][row] = entry + multiple * matrix[column][pivot_RowIndex]
 
-    print(matrix)
+
+    print("Transforming after part 2" + str(matrix))
     return matrix
 
